d8a2c17c10024c6fc61c56cc3fe92572b1d1114c
Kristina Camia <kcamia19@gmail.com>
2020-02-13 12:59:15 -0500

    started connect app but not right 22

--- js/connect-app.js
+++ js/connect-app.js
@@ -2,8 +2,32 @@
 
 ///////////////////// APP STATE (VARIABLES) /////////////////////////
 
+let board;
+
 ///////////////////// CACHED ELEMENT REFERENCES /////////////////////
 
 ///////////////////// EVENT LISTENERS ///////////////////////////////
 
+window.onload = init;
+const circles = Array.from(document.querySelectorAll("display"))
+
 ///////////////////// FUNCTIONS /////////////////////////////////////
+
+funtion init() {
+  board = [
+    "", "", "", "", "", "", "",
+    "", "", "", "", "", "", "",
+    "", "", "", "", "", "", "",
+    "", "", "", "", "", "", "",
+    "", "", "", "", "", "", "",
+    "", "", "", "", "", "", ""
+  ];
+
+  render();
+}
+
+function render() {
+  board.forEach(function(mark, index)) {
+    circles[index].textContent = mark;
+  });
+}
